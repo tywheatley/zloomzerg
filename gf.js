3fcb55a8ac5f801395831537588799faf380eb96
Ty Wheatley <tywheatley@gmail.com>
2012-09-24 08:08:19 -0700

    changes

--- gf.js
+++ gf.js
@@ -12,17 +12,30 @@ function print_stocks(input){
 function printPortfolio(portfolio){
 //alert("print portfolio");
 //calls printEntry on each entry
+	//$('#content_window').append($('<div class="container-fluid"><'))
 	for(var x in portfolio){
 		printEntry(portfolio[x]);
 	}
 }
 
 function printEntry(entry){
+	//alert(entry);
 //alert("Print Entry called");
 //prints individual stock entries
 	var pieces = entry.split(",");
-	$('#content_window').append($('<div class="stock"><span class="name">' +pieces[0]+' </span><span class="symbol">' + pieces[1] + '</span></div>')).addClass('stock');
+	//$('#content_window').append($('<div class="stock container" data-toggle="collapse" href="details"><span class="name collapse accordion-header"><a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#details">' +pieces[0]+' </a></span><span class="symbol">' + pieces[1] + '</span><div class="collapse" id="collapse" style="height: 0px;>'+'details'+'</div></div>')).addClass('stock');
+/*
+	$('#content_window').append($('<div class="accordion-group"><div class="accordion-heading"> \
+									<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="'+pieces[1]+'">'+ pieces[0] +'\
+									</a></div><div id="'+pieces[1]+'" class="accordion-body collapse" style="height: 0px; "><div class="accordion-inner">Details</div>\
+									</div></div>'))
+*/
 	//alert(pieces[0]);
 	//for(var y in pieces){
 		//alert(pieces[y]);
+	$('#accordion3').append($('<div class="accordion-group" id="group2">\
+							   		<div class="accordion-heading">\
+							   			<a class="accordion-toggle" data-toggle="collapse" data-parent="#group2" href="#'+pieces[1]+'">'+pieces[0]+'</a></div>\
+							   			<div id="#'+pieces[1]+'" class="accordion-body collapse">\
+							   			<div class="accordion-inner">Lorem ipsum div </dolor></div></div> '));
 }
